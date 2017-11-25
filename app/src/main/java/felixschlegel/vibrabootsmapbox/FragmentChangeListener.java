b66cfad14c4b8d85e69bda9db6ba8b3dd47853ff
pygoner <pygoner@gmail.com>
2017-11-25 21:11:39 +0100

    1.0.8
    - inplements switch fragments

---
+++ app/src/main/java/felixschlegel/vibrabootsmapbox/FragmentChangeListener.java
@@ -0,0 +1,11 @@
+package felixschlegel.vibrabootsmapbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by martinjobke on 25.11.17.
+ */
+
+public interface FragmentChangeListener {
+    public void replaceFragment(Fragment fragment);
+}
--- app/src/main/java/felixschlegel/vibrabootsmapbox/NavigationActivityFragment.java
+++ app/src/main/java/felixschlegel/vibrabootsmapbox/NavigationActivityFragment.java
@@ -1,16 +1,26 @@
 package felixschlegel.vibrabootsmapbox;
 
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class NavigationActivityFragment extends Fragment implements View.OnClickListener{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+
+        Button btn = (Button) getActivity().findViewById(R.id.btnStart);
+        btn.setOnClickListener(this);
+    }
 
     public NavigationActivityFragment() {
     }
@@ -23,6 +33,19 @@ public class NavigationActivityFragment extends Fragment implements View.OnClick
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnStart: {
+                showOtherFragment();
+                break;
+            }
+        }
 
+    }
+
+    public void showOtherFragment()
+    {
+        Fragment fr=new NavigationShoesFragment();
+        FragmentChangeListener fc=(FragmentChangeListener)getActivity();
+        fc.replaceFragment(fr);
     }
 }
