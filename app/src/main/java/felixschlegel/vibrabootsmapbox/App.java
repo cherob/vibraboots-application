ca56fb0986dd810e85f7ffffa97c1506e5682902
BerkaySalman <berkay-salman@outlook.de>
2017-11-26 10:07:14 +0100

    IP Scan Merge

---
+++ app/src/main/java/felixschlegel/vibrabootsmapbox/App.java
@@ -0,0 +1,27 @@
+package felixschlegel.vibrabootsmapbox;
+
+import android.app.Application;
+
+/**
+ * Created by robin on 25.11.2017.
+ */
+
+public class App extends Application {
+    private static App mInstance = null;
+    private ShoeCommunication mShoes;
+
+    public static App getInstance() {
+        return mInstance;
+    }
+
+    public ShoeCommunication getShoeCommunication(){
+        return mShoes;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        mShoes = new ShoeCommunication();
+    }
+}
